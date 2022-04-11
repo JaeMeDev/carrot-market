ab34bf18029a43611cb2a740c35cd20626aa2834
JaeMeDev <jaeme0406@gmail.com>
2022-04-11 19:29:48 +0900

    Tailwind Dark Mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,18 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
-      <div className="bg-white flex flex-col justify-between sm:bg-red-400 md:bg-teal-400 lg:bg-indigo-400 xl:bg-yellow-400 2xl:bg-pink-500 p-6 rounded-3xl shadow-xl">
-        <span className="font-semibold text-3xl">Select Item</span>
+    <div className="bg-slate-400 dark xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
+      <div className="bg-white dark:bg-black flex flex-col justify-between sm:bg-red-400 md:bg-teal-400 lg:bg-indigo-400 xl:bg-yellow-400 2xl:bg-pink-500 p-6 rounded-3xl shadow-xl">
+        <span className="font-semibold dark:text-white text-3xl">
+          Select Item
+        </span>
         <ul>
           {[1, 2, 3, 4].map((i) => (
-            <div
-              key={i}
-              className="flex justify-between my-2 odd:bg-blue-50 even:bg-yellow-500"
-            >
-              <span className="text-gray-500">Grey Chair</span>
-              <span className="font-semibold">$19</span>
+            <div key={i} className="flex justify-between my-2">
+              <span className="text-gray-500 dark:text-gray-100">
+                Grey Chair
+              </span>
+              <span className="font-semibold dark:text-white">$19</span>
             </div>
           ))}
         </ul>
@@ -30,9 +31,9 @@ const Home: NextPage = () => {
         </div>
 
         <button
-          className="mt-5 bg-blue-500 text-white p-3
+          className="mt-5 dark:bg-black dark:border-white dark:border bg-blue-500 text-white p-3
         text-center rounded-xl w-3/4 block mx-auto 
-        hover:bg-teal-500 hover:text-black
+        hover:bg-teal-500  dark:hover:bg-black dark:hover:text-white hover:text-black
         active:bg-yellow-500 focus:bg-red-500"
         >
           Checkout
