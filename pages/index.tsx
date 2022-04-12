b5df10e38205bc782ea02b9c4457100649c47938
JaeMeDev <jaeme0406@gmail.com>
2022-04-12 10:20:04 +0900

    feat: Auth part One

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div className="bg-slate-400 dark xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
-      <div className="bg-[url('/vercel.svg')]"></div>
-     <h2 className="text-[1000px] text-[#000]">Hello</h2>
-    </div>
-  );
+  return null;
 };
 
 export default Home;
