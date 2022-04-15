29b1af4a1d6059adae442eb18e325aa13be82839
JaeMeDev <jaeme0406@gmail.com>
2022-04-15 15:47:11 +0900

    feat: Clean Code part Two

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -12,8 +12,7 @@ interface EnterForm {
 }
 
 const Enter: NextPage = () => {
-  const [] = useMutation();
-  const [submitting, setSubmitting] = useState(false);
+  const [enter, { loading, data, error }] = useMutation("/api/users/enter");
   const { register, handleSubmit, reset } = useForm<EnterForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => {
@@ -24,18 +23,12 @@ const Enter: NextPage = () => {
     reset();
     setMethod("phone");
   };
-  const onValid = (data: EnterForm) => {
-    setSubmitting(true);
-    fetch("/api/users/enter", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then(() => {
-      setSubmitting(false);
-    });
+  const onValid = (validForm: EnterForm) => {
+    if (loading) return;
+    enter(validForm);
   };
+
+  console.log(loading, data, error);
 
   return (
     <div className="mt-16 px-4">
@@ -92,10 +85,10 @@ const Enter: NextPage = () => {
             />
           ) : null}
           {method === "email" ? (
-            <Button text={submitting ? "Loading" : "Get login link"} />
+            <Button text={loading ? "Loading" : "Get login link"} />
           ) : null}
           {method === "phone" ? (
-            <Button text={submitting ? "Loading" : "Get one-time password"} />
+            <Button text={loading ? "Loading" : "Get one-time password"} />
           ) : null}
         </form>
         <div className="mt-8">
