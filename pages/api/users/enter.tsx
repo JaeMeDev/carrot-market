0c60088d5a4076de49dbdfd80f2b786354edd10f
JaeMeDev <jaeme0406@gmail.com>
2022-04-17 17:40:32 +0900

    feat: Sending Email

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,7 +1,10 @@
+import mail from "@sendgrid/mail";
 import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
+
+mail.setApiKey(process.env.SENDGRID_KEY!);
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
@@ -41,6 +44,15 @@ async function handler(
       body: `Your login token is ${payload}.`,
     });
     console.log(message);
+  } else if (email) {
+    const email = await mail.send({
+      from: "jaeme0406@gmail.com",
+      to: "devrappers@gmail.com",
+      subject: "Your Carrot Market Verification Email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(email);
   }
 
   return res.json({
