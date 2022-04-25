65b41c16df415b487b936844148679455d833081
JaeMeDev <jaeme0406@gmail.com>
2022-04-25 13:34:59 +0900

    feat:Responses and Redirections

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,12 @@
-import { NextFetchEvent, NextRequest } from "next/server";
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log(req.ua);
+  if (req.ua?.isBot) {
+    return new Response("Plz don't be a bot. Be human", { status: 403 });
+  }
+  if (!req.url.includes("/api")) {
+    if (!req.url.includes("/enter") && !req.cookies.carrotsession) {
+      return NextResponse.redirect("/enter");
+    }
+  }
 }
